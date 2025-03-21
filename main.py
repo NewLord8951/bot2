4bab5ca810fecdb9617c2b10cee826c875c4ea55
New_Lord_8951 <alexandrmaniraki@gmail.com>
2025-03-21 13:21:22 +0300

    Манираки

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-import os
 import asyncio
+import os
+import sqlite3
 from loguru import logger
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from dotenv import find_dotenv, load_dotenv
+
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv("TOKEN")
-bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+
+def add_user(user_id, username):
+    conn = sqlite3.connect('hacker.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (\
+        user_id, username) VALUES (?, ?)", (user_id, username))
+        conn.commit()
+        print(f'Пользователь {username} ({user_id}) добавлен в базу данных.')
+    except sqlite3.IntegrityError as e:
+        print(e)
+    finally:
+        conn.close()
 
 
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Привеееет!')
+    await message.answer('Здорово корова!')
     logger.info(f'Пользователь {(
         message.from_user.full_name, message.from_user.id)}, запустил бота')
+    username = message.from_user.username
+    user_id = message.from_user.id
+
+    add_user(user_id, username)
+
+
+@dp.message(Command("show"))
+async def show_info(message: types.Message):
+    conn = sqlite3.connect('hacker.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM hacker")
+    rows = cursor.fetchall()
+    for row in rows:
+        logger.info(row)
+        await message.answer(f"{row}")
 
 
 @dp.message()
 async def echo(message: types.Message):
-    text = message.text
-
-    if text in ['Привет', 'привет', 'hi', 'hello']:
-        await message.answer('И тебе привеееееееет!')
-    elif text in ['Пока', 'пока', 'До свидания']:
-        await message.answer('И тебе пока!, (печально что ты уходишь...)')
-    else:
-        await message.answer(message.text)
+    await message.answer(message.text)
 
 
 async def main():
@@ -37,6 +59,16 @@ async def main():
                rotation='3 days',
                backtrace=True,
                diagnose=True)
+    conn = sqlite3.connect('hacker.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS hacker (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        user_id INTEGER NOT NULL
+    )
+    ''')
+    bot = Bot(token=os.getenv('TOKEN'))
     await dp.start_polling(bot)
 
 asyncio.run(main())
